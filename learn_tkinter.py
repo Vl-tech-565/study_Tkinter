56e0fe1e503b7b9f0867149c6b256faeb4fde6bf
Vl-tech-565 <Ducanto11@yandex.ru>
2022-09-03 14:05:54 +0300

    Added the second lesson

--- learn_tkinter.py
+++ learn_tkinter.py
@@ -15,4 +15,20 @@ root.geometry(init_sizes+offset)
 root.title('My first GUI app')
 root.resizable(False, False)
 
+label_1 = tk.Label(root, text='''Hello,
+world!''',
+                   bg = 'light cyan',           # background
+                   fg= 'dark cyan',             # font-ground
+                   font=('Arial', 20, 'italic'),
+                   padx=0,                      # paddle x-axis
+                   pady=0,                      # paddle y-axis
+                   width=10,
+                   height=14,
+                   anchor='center',             # location of the text
+                   # relief=tk.RAISED,            # border
+                   # bd=1,                        # border depth
+                   justify=tk.CENTER            # align the string
+                   )
+label_1.pack()                                  # show the label
+
 root.mainloop()
