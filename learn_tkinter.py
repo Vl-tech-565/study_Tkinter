47dd4f2d7793e98364df0e76bd9fd7a2cbdbe7fa
Vl-tech-565 <Ducanto11@yandex.ru>
2022-09-04 13:41:02 +0300

    Homework

--- learn_tkinter.py
+++ learn_tkinter.py
@@ -1,34 +1,14 @@
-# 3rd lesson. Button widget
+# 3rd lesson. Homework
 
 import tkinter as tk
+import random
 
-# set variables
-count = 0
-is_disabled = False
-
-
-def change_4th_button():
-    global is_disabled
-    if is_disabled:
-        btn4['state'] = tk.NORMAL
-        is_disabled = False
-    else:
-        btn4['state'] = tk.DISABLED
-        is_disabled = True
+colors = ('black', 'white', 'cyan', 'gray', 'purple', 'pink', 'orange', 'light yellow')
 
 
-def add_label():
-    label = tk.Label(root, text='New label',
-                     bg='gray',
-
-                     )
-    label.pack()
-
-
-def counter():
-    global count
-    count += 1
-    btn4['text'] = f'Counter: {count}'
+def change_bg_color():
+    global colors
+    root['bg'] = colors[random.randint(0, len(colors) - 1)]
 
 
 # init root window
@@ -44,28 +24,10 @@ root.geometry(init_sizes + offset)
 root.title('My first GUI app')
 root.resizable(False, False)
 
-# Test buttons
-btn1 = tk.Button(root, text='disable 4th',
-                 command=change_4th_button
-                 )
-
-btn1.pack()
-
-btn2 = tk.Button(root, text='Add label',
-                 command=add_label
-                 )
-btn2.pack()
-
-btn3 = tk.Button(root, text='Add label by lambda',
-                 command=lambda: tk.Label(root, text='new lambda').pack()
-                 )
-btn3.pack()
-
-btn4 = tk.Button(root, text=f'Counter: {count}',
-                 command=counter,
-                 activebackground='dark cyan',
-                 bg='cyan'
-                 )
-btn4.pack()
-
+# Test button
+btn_chg_clr = tk.Button(text='change bg color',
+                        bg='light green',
+                        command=change_bg_color
+                        )
+btn_chg_clr.pack(pady=150)
 root.mainloop()
